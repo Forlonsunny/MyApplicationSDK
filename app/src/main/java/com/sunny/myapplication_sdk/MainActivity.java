a37b864382de9b5f0c0d117cf161c5f2ed23c466
Mahadi Hasan Sunny <sunny_mhs@hotmail.com>
2015-05-13 21:48:38 +0600

    is

--- app/src/main/java/com/sunny/myapplication_sdk/MainActivity.java
+++ app/src/main/java/com/sunny/myapplication_sdk/MainActivity.java
@@ -7,7 +7,7 @@ import android.view.MenuItem;
 
 
 public class MainActivity extends ActionBarActivity {
-
+//This is a test
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
